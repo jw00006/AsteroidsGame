cae0b130ca863078b07bbe5300a011f00e1d8638
jw00006 <jacobawilliamson1@gmail.com>
2025-02-03 15:16:39 -0500

    Added asteroid.py file and updated main to import it

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
 
 def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	bg_color = (0, 0, 0) #BLACK
+
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+
+	Asteroid.containers = (asteroids, updatable, drawable)
 	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 	updatable.add(player)
 	drawable.add(player)
